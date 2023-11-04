4d404b67a338e9789ee1ffa9433d01639f668735
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-04 18:52:58 -0400

    Added Json

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const http = require('http'); 
+const url = require('url'); 
 
 //creates a server
 const server = http.createServer((req,res)=> {
-    res.end('Hello from the server');
+
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview'){
+    res.end('This is the overview page');
+    }
+    else if(pathName === '/product'){
+        res.end('This is the product page');
+    }
+    else{
+        //in case of a page not found
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-header':'Yo!'
+    });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
+
+
 
 server.listen(8000,'127.0.0.1', ()=>{
     console.log('Listening...')
